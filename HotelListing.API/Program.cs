37753b23787aeccb31f2096383e40adef2f34ee8
Rohit Kumar <ROHITKUMARJAJORIYA@GMAIL.COM>
2023-06-06 14:45:49 +0530

    caching

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -89,6 +89,12 @@ builder.Services.AddAuthentication( options =>
 
 // End - Setting Authentication process
 
+builder.Services.AddResponseCaching(options =>
+{
+    options.MaximumBodySize = 1024;
+    options.UseCaseSensitivePaths = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -103,6 +109,19 @@ app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
+
+app.UseResponseCaching();
+
+app.Use(async (context, next) => {
+    context.Response.GetTypedHeaders().CacheControl = new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+    {
+        Public = true,
+        MaxAge = TimeSpan.FromSeconds(10),
+    };
+    context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] = new string[] { "Accept-Encoding" };
+
+    await next();
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
